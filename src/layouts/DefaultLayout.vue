dcd3f6d5cae290c18de7e63777f82d535e9ceec8
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-21 19:37:24 +0600

    Basic layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>DefaultLayout</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  // Add your component options here
+};
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
