4079b1df7f9dac01356ad847bce90d4a6c778b5b
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-22 00:35:06 +0600

    Login UI

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -1,1 +1,172 @@
-<template>Login Page</template>
+<template>
+  <div class="auth-page d-flex align-items-center min-vh-100">
+    <div class="container-fluid p-0">
+      <div class="row g-0">
+        <div class="col-xxl-3 col-lg-4 col-md-5">
+          <div class="d-flex flex-column h-100 py-5 px-4">
+            <div class="text-center text-muted mb-2">
+              <div class="pb-3">
+                <a href="#">
+                  <span class="logo-lg">
+                    <img src="@/assets/images/logo-sm.svg" alt="" height="24" />
+                    <span class="logo-txt auth-logo-txt">HalKhata</span>
+                  </span>
+                </a>
+                <p class="text-muted font-size-15 w-75 mx-auto mt-3 mb-0">
+                  User Experience & Interface Design Strategy Saas Solution
+                </p>
+              </div>
+            </div>
+
+            <div class="my-auto">
+              <div class="p-3 text-center">
+                <img
+                  src="@/assets/images/auth-img.png"
+                  alt=""
+                  class="img-fluid"
+                />
+              </div>
+            </div>
+
+            <div class="mt-4 mt-md-5 text-center">
+              <p class="mb-0">
+                &copy; {{ currentYear }} HalKhata Inventory | Developed with
+                <i class="mdi mdi-heart text-danger"></i> by MokammelTanvir.
+              </p>
+            </div>
+          </div>
+
+          <!-- end auth full page content -->
+        </div>
+        <!-- end col -->
+
+        <div class="col-xxl-9 col-lg-8 col-md-7">
+          <div class="auth-bg bg-light py-md-5 p-4 d-flex">
+            <div class="bg-overlay-gradient"></div>
+            <!-- end bubble effect -->
+            <div
+              class="row justify-content-center g-0 align-items-center w-100"
+            >
+              <div class="col-xl-4 col-lg-8">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="px-3 py-3">
+                      <div class="text-center">
+                        <h5 class="mb-0">Welcome!</h5>
+                        <p class="text-muted mt-2">Sign in to continue.</p>
+                      </div>
+                      <form class="mt-4 pt-2">
+                        <div class="form-floating form-floating-custom mb-3">
+                          <input
+                            type="text"
+                            class="form-control"
+                            id="input-username"
+                            placeholder="Enter User Name"
+                          />
+                          <label for="input-username">Username</label>
+                          <div class="form-floating-icon">
+                            <i class="uil uil-users-alt"></i>
+                          </div>
+                        </div>
+                        <div
+                          class="form-floating form-floating-custom mb-3 auth-pass-inputgroup"
+                        >
+                          <input
+                            type="password"
+                            class="form-control"
+                            id="password-input"
+                            placeholder="Enter Password"
+                          />
+                          <button
+                            type="button"
+                            class="btn btn-link position-absolute h-100 end-0 top-0"
+                            id="password-addon"
+                          >
+                            <i
+                              class="mdi mdi-eye-outline font-size-18 text-muted"
+                            ></i>
+                          </button>
+                          <label for="password-input">Password</label>
+                          <div class="form-floating-icon">
+                            <i class="uil uil-padlock"></i>
+                          </div>
+                        </div>
+                        <div
+                          class="form-check form-check-primary font-size-16 py-1"
+                        >
+                          <input
+                            class="form-check-input"
+                            type="checkbox"
+                            id="remember-check"
+                          />
+                          <div class="float-end">
+                            <a
+                              href="auth-resetpassword-basic.html"
+                              class="text-muted text-decoration-underline font-size-14"
+                              >Forgot your password?</a
+                            >
+                          </div>
+                          <label
+                            class="form-check-label font-size-14"
+                            for="remember-check"
+                          >
+                            Remember me
+                          </label>
+                        </div>
+
+                        <div class="mt-3">
+                          <button class="btn btn-primary w-100" type="submit">
+                            Log In
+                          </button>
+                        </div>
+                      </form>
+                      <!-- end form -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end col -->
+      </div>
+      <!-- end row -->
+    </div>
+    <!-- end container fluid -->
+  </div>
+  <!-- end authentication section -->
+</template>
+
+<script setup>
+/* All Libraries Imports */
+import { ref, reactive } from "vue";
+import { useAuthStore } from "@/store/auth";
+import { useRouter } from "vue-router";
+
+/* All Instance */
+const router = useRouter();
+const authStore = useAuthStore();
+
+/* All Variables */
+const LoginForm = reactive({
+  email: "",
+  password: "",
+});
+
+/* All Methods */
+const login = () => {
+  //   console.log("Login");
+};
+
+/* Hooks and Computed */
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+    };
+  },
+};
+</script>
