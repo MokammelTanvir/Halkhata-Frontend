7e7e926af8b46796a5cd95ea91958e93682eebc3
Mokammel Hossain Tanvir <mokammeltanvir01@gmail.com>
2024-03-22 01:42:40 +0600

    sweetalert2

--- src/main.js
+++ src/main.js
@@ -5,11 +5,17 @@ import App from "./App.vue";
 import router from "./router";
 
 import VeeValidatePlugin from "./plugins/vee-validate";
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(VeeValidatePlugin);
+app.use(VueSweetAlert2, {
+  confirmButtonColor: "#A95EEA",
+  cancelButtonColor: "#FF7674",
+});
 
 app.mount("#app");
